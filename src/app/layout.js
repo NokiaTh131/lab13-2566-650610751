9961cb3ed5a02923acdd996e7ef2ae432caca7ee
= <=>
2023-08-07 15:46:18 +0700

    GGEZ

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Jakkapong Jinasen"
+            studentId="650610751"
           />
         </div>
       </body>
